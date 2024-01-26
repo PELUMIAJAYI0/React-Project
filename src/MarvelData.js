c71fe4957bf81b6505451e7f923470fa3dcba8b8
PELUMIAJAYI0 <ajayioluwapelumi0001@gmail.com>
2024-01-26 14:49:19 -0800

    Project for the react code

---
+++ src/MarvelData.js
@@ -0,0 +1,116 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "Charles Babbage",
+    alias: "Father of Computer",
+    img1: "https://d8it4huxumps7.cloudfront.net/uploads/images/619b87deb1138_untitled_design_64.png",
+    homeworld: "Earth",
+    abilities: [
+      "Genius-level intellect",
+      "Powered armor suit",
+      "Superhuman strength",
+    ],
+    team: "Programmers",
+    description:
+      "Tony Stark, also known as Iron Man, is a brilliant inventor, philanthropist, and industrialist. He uses his genius-level intellect and a highly advanced powered armor suit to fight for justice and protect the world from various threats. Stark is a charismatic playboy turned superhero who combines his technological prowess with unwavering determination to uphold the values of heroism and innovation.",
+  },
+  {
+    name: "Brendan Eich",
+    alias: "Father of Javascript",
+    abilities: ["Powered armor suit", "Advanced technology skills"],
+    team: "Programmers",
+    img1: "	https://d8it4huxumps7.cloudfront.net/uploads/images/619b87841cecd_untitled_design_63.png",
+    homeworld: "Earth",
+    description:
+      "Pepper Potts, the formidable Rescue, is an accomplished businesswoman and technological genius. She dons a powered armor suit equipped with an array of advanced technology and weaponry, allowing her to stand alongside the Avengers in the battle against supervillains and global threats. Pepper is a determined and fearless heroine with an unwavering commitment to justice.",
+  },
+  {
+    name: "Tim Berners-Lee",
+    alias: "Father of World-WideWeb",
+    abilities: ["Powered armor suit", "Military expertise"],
+    team: "Programmers",
+    img1: "https://d8it4huxumps7.cloudfront.net/uploads/images/619b8743ecd49_untitled_design_62.png",
+    homeworld: "Earth",
+
+    description:
+      "James 'Rhodey' Rhodes, also known as War Machine, is a highly skilled pilot and a military expert. He operates a heavily armed powered armor suit that enhances his strength and combat capabilities. With a strong sense of duty and loyalty to his allies, Rhodey plays a crucial role in the Avengers' efforts to maintain peace and protect the world.",
+  },
+  {
+    name: "Linus Torvalds ",
+    alias: "Designer of LINUX kernel and Inventor of Git Version Control System",
+    abilities: ["Powered armor suit", "Genius-level intellect"],
+    team: "Programmers",
+    img1: "https://i.pinimg.com/736x/e3/02/9b/e3029b056f1deaf2011d72d0bce0bafa.jpg",
+    homeworld: "Earth",
+
+    description:
+      "Riri Williams, known as Ironheart, is a prodigious young inventor with a genius-level intellect. She has designed her own advanced powered armor suit, which she uses to fight against threats as a member of the Young Avengers. Riri's intelligence, determination, and innovative spirit make her a rising star in the superhero community.",
+  },
+
+  // Spider-Man
+  {
+    name: "Brian Kernighan",
+    alias: "Collaborator developer of UNIX in Bells Lab",
+    abilities: ["Bio-electrokinesis", "Wall-crawling"],
+    team: "Champions",
+    img1: "https://d8it4huxumps7.cloudfront.net/uploads/images/619b8959323e3_untitled_design_66.png",
+    homeworld: "Earth",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "Miles Morales, the teenage Spider-Man, has bio-electrokinesis powers, in addition to wall-crawling abilities. As a member of the Champions, Miles embodies the spirit of the Spider-Man legacy, fighting for equality and justice in the Marvel Universe while navigating the challenges of being a young hero.",
+  },
+
+  {
+    name: " Ryan Dahl",
+    alias: "Inventor of Node.JS",
+    abilities: ["Web-shooting", "Superhuman agility"],
+    team: "Agents of Atlas",
+    img1: "	https://d8it4huxumps7.cloudfront.net/uploads/images/619b869a3c7a1_untitled_design_60.png",
+    homeworld: "Earth",
+    description:
+      "Cindy Moon, codenamed Silk, is a skilled hero with the power of web-shooting and remarkable superhuman agility. She is an essential member of the Agents of Atlas, using her arachnid-like abilities to combat threats and safeguard the world.",
+  },
+
+  {
+    name: "Bjarne Stroustrup",
+    alias: "Developer and creator of C++ Proramming Language",
+    abilities: ["Peak human strength", "Vibranium shield"],
+    team: "Avengers",
+    img1: "	https://d8it4huxumps7.cloudfront.net/uploads/images/619b88804d6d2_untitled_design_67.png",
+    homeworld: "Earth",
+    description:
+      "Steve Rogers, Captain America, is the embodiment of honor and virtue. Enhanced with peak human strength and wielding a Vibranium shield, he stands as a symbol of justice and resilience. Steve's unwavering commitment to the ideals of heroism makes him a cornerstone of the Avengers and the Marvel Universe.",
+  },
+  {
+    name: " Donald Knuth",
+    alias: "Father of Analysis of Alogrithm",
+    abilities: ["Wingsuit", "Telepathic link with birds"],
+    team: "Avengers",
+    img1: "	https://d8it4huxumps7.cloudfront.net/uploads/images/619b86fc2ca98_untitled_design_61.png",
+    homeworld: "Earth",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+  {
+    name: " Dennis Ritchie",
+    alias: "Created the C programming Language",
+    abilities: ["Wingsuit", "Telepathic link with birds"],
+    team: "Avengers",
+    img1: "	https://d8it4huxumps7.cloudfront.net/uploads/images/619b8908369ac_untitled_design_69.png",
+    homeworld: "Earth",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+  {
+    name: "James Gosling",
+    alias: "Created the JAVA programming Language",
+    abilities: ["Wingsuit", "Telepathic link with birds"],
+    team: "Avengers",
+    img1: "		https://d8it4huxumps7.cloudfront.net/uploads/images/619b88b9187d2_untitled_design_68.png",
+    homeworld: "Earth",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+];
+
+console.log(characters);
